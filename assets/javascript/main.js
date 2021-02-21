58b89bc7a61ee6004aa23d082a8de3feb7e1554f
smeghen <smeghen@gmail.com>
2021-02-21 20:51:33 +0000

    Alerts added for not selecting a Game type or Tables number

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -3,10 +3,10 @@ function getGameType(){
                 var gameType = document.getElementById("gameType");
                 var result1 = gameType.options[gameType.selectedIndex].value;
                 if (result1 === "Choose a Game to Play" ){
-                 document.getElementById("result1").innerHTML = "Not valid input";
+                 alert("Not valid input for Game Type");
                 }
                 else{
-                document.getElementById("result1").innerHTML = result1;
+                return result1;
                 }
             }
 
@@ -14,9 +14,9 @@ function getTableNumber(){
                 var tableNumber = document.getElementById("tableNumber");
                 var result2 = tableNumber.options[tableNumber.selectedIndex].value;
                  if (result2 === "Choose the tables for the Game" ){
-                 document.getElementById("result2").innerHTML = "Not valid input";
+                 alert("Not valid input for Tables number");
                 }
                 else{                
-                document.getElementById("result2").innerHTML = result2;
+                return result2;
                 }
             }
