01a61422ed26a6fe4227960b9d41e51abd5f6a0b
smeghen <smeghen@gmail.com>
2021-02-21 21:35:12 +0000

    Had to move to a third modal for game play instead of a separate page to simplify using data from games selection modal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,22 +1,25 @@
 /* Code found to get inputs from dropdown menu, modified to my needs.  https://mkyong.com/javascript/javascript-get-selected-value-from-dropdown-list/ */  
 function getGameType(){
                 var gameType = document.getElementById("gameType");
-                var result1 = gameType.options[gameType.selectedIndex].value;
-                if (result1 === "Choose a Game to Play" ){
+                var type = gameType.options[gameType.selectedIndex].value;
+                if (type === "Choose a Game to Play" ){
                  alert("Not valid input for Game Type");
                 }
                 else{
-                return result1;
+                    
+                return type;
                 }
             }
 
 function getTableNumber(){
                 var tableNumber = document.getElementById("tableNumber");
-                var result2 = tableNumber.options[tableNumber.selectedIndex].value;
-                 if (result2 === "Choose the tables for the Game" ){
+                var number = tableNumber.options[tableNumber.selectedIndex].value;
+                 if (number === "Choose the tables for the Game" ){
                  alert("Not valid input for Tables number");
                 }
-                else{                
-                return result2;
+                else{     
+                    document.getElementById("operand2").innerHTML = number;
+                               
+                return number;
                 }
             }
