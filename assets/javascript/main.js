5010c5f7ff2febd1a804006590f436af6ffea7b8
smeghen <smeghen@gmail.com>
2021-02-23 21:20:49 +0000

    tidied up the styling of the games selection modal after validation of selection

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -48,26 +48,31 @@ function validateInput() {
             var letsgobutton = document.getElementById("letsgo");
             var type = document.getElementById("type");
             var selection = document.getElementById("selection");
+            var home = document.getElementById("home");
 
             if (validatebutton.style.display === "none"           
              )
                {
                 validatebutton.style.display = "block";
                 type.style.display = "block";
+                home.style.display = "block";
                 
             } else {
                 validatebutton.style.display = "none";
                 type.style.display = "none";
+                home.style.display = "none";
                 
            
             }
             if (letsgobutton.style.display === "block") {
                 letsgobutton.style.display = "none";
                 selection.style.display = "none";
+                
             }
             else {
                 letsgobutton.style.display = "block";
                 selection.style.display = "block";
+                
             }
 
         }
