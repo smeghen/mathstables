02a1072259a416dc42d339264a085a86a089c2f1
smeghen <smeghen@gmail.com>
2021-02-24 16:45:57 +0000

    Added js function to generate random number between 1 and 12, then display on Game modal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,4 +1,5 @@
 /* Code found to get inputs from dropdown menu, modified to my needs.  https://mkyong.com/javascript/javascript-get-selected-value-from-dropdown-list/ */
+/* Function takes the game type selection from dropdown menu, checks it and displays the corresponding maths symbol on Game Modal*/
 function getGameType() {
     var gameType = document.getElementById("gameType");
     var type = gameType.options[gameType.selectedIndex].value;
@@ -15,7 +16,7 @@ function getGameType() {
          document.getElementById("operand3").innerHTML = "%";
     }
 }
-
+/* Function takes the number selection from dropdown menu, and displays it on Game Modal*/
 function getTableNumber() {
     var tableNumber = document.getElementById("tableNumber");
     var number = tableNumber.options[tableNumber.selectedIndex].value;
@@ -24,6 +25,8 @@ function getTableNumber() {
 
 
 /*    Code for validating dropdown menu selection found on Stackoverflow and modified https://stackoverflow.com/questions/15371162/javascript-dropdown-validation-and-alert */
+/*Function checks that a validate input has been selected from the dropdown menus and returns message if not. For valid inputs it hides buttons and dropdown and displays selection 
+new button to take user to game*/
 function validateInput() {
     var gameType = document.getElementById("gameType");
     var type = gameType.options[gameType.selectedIndex].value;
@@ -42,37 +45,35 @@ function validateInput() {
             var type = document.getElementById("type");
             var selection = document.getElementById("selection");
             var home = document.getElementById("home");
-
-            if (validatebutton.style.display === "none"           
-             )
-               {
+        if (validatebutton.style.display === "none") {
                 validatebutton.style.display = "block";
                 type.style.display = "block";
-                home.style.display = "block";
-                
+                home.style.display = "block";      
             } else {
                 validatebutton.style.display = "none";
                 type.style.display = "none";
                 home.style.display = "none";
-                
-           
             }
             if (letsgobutton.style.display === "block") {
                 letsgobutton.style.display = "none";
                 selection.style.display = "none";
-                
             }
             else {
                 letsgobutton.style.display = "block";
                 selection.style.display = "block";
-                
             }
-
         }
         hideStart();
         document.getElementById("selectedType").innerHTML = type;
         document.getElementById("selectedNumber").innerHTML = number;
     }
+}
+
+/*  Random number generator between 1 and 12. Display on game */
+document.getElementById("startgame").addEventListener("click", displayRandomNumber);
+function displayRandomNumber() {
+    let firstNum = parseInt(Math.random() * 12);
+    document.getElementById("operand1").textContent = firstNum;
 }
 
 
