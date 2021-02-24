8998ee6b5f82ee26234197ce8629140c29e7fba2
smeghen <smeghen@gmail.com>
2021-02-24 13:37:30 +0000

    add functions getGameType and getTableNumber to take selection input and display on game modal

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -2,31 +2,24 @@
 function getGameType() {
     var gameType = document.getElementById("gameType");
     var type = gameType.options[gameType.selectedIndex].value;
-    if (type === "Choose a Game to Play") {
-        alert("Not valid input for Game Type");
-
+    if (type === "Addition") {
+         document.getElementById("operand3").innerHTML = "+";
     }
-    else {
-        /*if else loop to get +-*% Symbol on game modal
-        if type === add getElementById = +
-        if type === sub getElementById = -
-        */
-
-        return type;
+    else if (type === "Subtraction") {
+         document.getElementById("operand3").innerHTML = "-";
+    }
+    else if (type === "Multiplication") {
+         document.getElementById("operand3").innerHTML = "x";
+    }
+    else if(type === "Division") {
+         document.getElementById("operand3").innerHTML = "%";
     }
 }
 
 function getTableNumber() {
     var tableNumber = document.getElementById("tableNumber");
     var number = tableNumber.options[tableNumber.selectedIndex].value;
-    if (number === "Choose the tables for the Game") {
-        alert("Not valid input for Tables number");
-    }
-    else {
         document.getElementById("operand2").innerHTML = number;
-
-        return number;
-    }
 }
 
 
